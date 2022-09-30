176081adaa0e8fadf22c01dd0e5df23858b4c6cb
Joshua Lattke <joshualattke@hotmail.com>
2022-09-30 15:46:49 -0500

    avence en el transcurso de la clase

---
+++ src/model/Player.java
@@ -0,0 +1,8 @@
+packge model;
+public class Player{
+private nickName;
+private name;
+private initialScore;
+private numberofLives;
+
+}
---
+++ src/ui/VideoGameManager.java
@@ -0,0 +1,10 @@
+packge ui;
+public class VideoGameManger{
+    public static void main(Strign[] args){
+        menu();
+    }
+    public static void menu{
+        System.out.println("Bienvenidos al juego"):
+        boo
+    }
+}
