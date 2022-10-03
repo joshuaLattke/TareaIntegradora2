4a023d11385edaabee584ef0ef773c44ad6b0d4c
Joshua Lattke <joshualattke@hotmail.com>
2022-10-02 21:14:54 -0500

    entrega 1 de la integradora

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,8 +1,35 @@
-packge model;
+package model;
 public class Player{
-private nickName;
-private name;
-private initialScore;
-private numberofLives;
+private String nickName;
+private  String  name;
+private  int initialScore;
+private  int numberofLives;
 
+public String getNickName() {
+    return nickName;
+}
+
+public void setNickName(String nickName) {
+    this.nickName = nickName;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+
+public int getInitialScore() {
+    return initialScore;
+}
+public void setInitialScore(int initialScore) {
+    this.initialScore = initialScore;
+}
+public int getNumberofLives() {
+    return numberofLives;
+}
+public void setNumberofLives(int numberofLives) {
+    this.numberofLives = numberofLives;
+}
+public Player
 }
--- src/ui/VideoGameManager.java
+++ src/ui/VideoGameManager.java
@@ -1,10 +1,21 @@
-packge ui;
+import java.util.logging.Level;
+
+package ui;
 public class VideoGameManger{
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(Strign[] args){
         menu();
     }
     public static void menu{
-        System.out.println("Bienvenidos al juego"):
-        boo
+        System.out.println("Bienvenidos al juego");
+
+        boolean exit=false;
+        while(!exit){
+            System.out.println("Digite una de las opcinones\n ");
+            int opcinion=nextInt();
+            
+        }
+
     }
 }
